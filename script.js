1ada66409ca08392c84d7519f582145d2c380ac5
Oghenetega16 <sukuruoghenetega@gmail.com>
2024-10-30 12:47:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', function(){
+
+    const slideCount = document.querySelectorAll('#slider-wrapper ul li').length;
+    const slideWidth = document.querySelector('#slider-wrapper').offsetWidth;
+    const totalWidth = slideCount * slideWidth + 'px';
+    const slider = document.querySelector('#slider-wrapper ul');
+    const next = document.querySelector('#next');
+    const previous = document.querySelector('#prev');
+
+    let leftPosition = 0;
+    let counter = 0;
+    slider.style.width = totalWidth;
+
+    next.addEventListener('click', (event) => {
+        event.preventDefault();
+        counter++;
+        if (counter == slideCount) {
+            counter = 0;
+            slider.style.left = leftPosition + 'px';
+        }
+        else {
+            leftPosition = `-${counter * slideWidth}px`;
+            slider.style.left = leftPosition;
+        }
+    });
+
+    previous.addEventListener("click", (event) => {
+        event.preventDefault();
+        counter--;
+
+        if (counter < 0) {
+            counter = slideCount - 1;
+            leftPosition = `-${counter * slideWidth}px`;
+            slider.style.left = leftPosition;
+        }
+        else {
+            leftPosition = `-${counter * slideWidth}px`;
+            slider.style.left = leftPosition;
+        }
+    });
+});
