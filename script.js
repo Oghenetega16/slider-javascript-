986d910345f05fa8c1caefc61b938143fa4dfae4
Oghenetega16 <sukuruoghenetega@gmail.com>
2024-11-04 11:12:39 +0100

    first commit

--- script.js
+++ script.js
@@ -4,15 +4,14 @@ window.addEventListener('load', function(){
     const slideWidth = document.querySelector('#slider-wrapper').offsetWidth;
     const totalWidth = slideCount * slideWidth + 'px';
     const slider = document.querySelector('#slider-wrapper ul');
-    const next = document.querySelector('#next');
-    const previous = document.querySelector('#prev');
 
     let leftPosition = 0;
     let counter = 0;
     slider.style.width = totalWidth;
 
-    next.addEventListener('click', (event) => {
-        event.preventDefault();
+    let timer = setInterval(slides, 2000);
+
+    function slides() {
         counter++;
         if (counter == slideCount) {
             counter = 0;
@@ -22,20 +21,13 @@ window.addEventListener('load', function(){
             leftPosition = `-${counter * slideWidth}px`;
             slider.style.left = leftPosition;
         }
+    }
+
+    slider.addEventListener('mouseover', function(){
+        clearInterval(timer)
     });
 
-    previous.addEventListener("click", (event) => {
-        event.preventDefault();
-        counter--;
-
-        if (counter < 0) {
-            counter = slideCount - 1;
-            leftPosition = `-${counter * slideWidth}px`;
-            slider.style.left = leftPosition;
-        }
-        else {
-            leftPosition = `-${counter * slideWidth}px`;
-            slider.style.left = leftPosition;
-        }
+    slider.addEventListener('mouseout', function(){
+        timer = setInterval(slides, 2000);
     });
 });
