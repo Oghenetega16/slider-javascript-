6b36e9b54f98dbf73e610b713f9cbe2eabe844d7
Oghenetega Sukuru <70195314+Oghenetega16@users.noreply.github.com>
2024-11-06 15:51:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,30 +4,37 @@ window.addEventListener('load', function(){
     const slideWidth = document.querySelector('#slider-wrapper').offsetWidth;
     const totalWidth = slideCount * slideWidth + 'px';
     const slider = document.querySelector('#slider-wrapper ul');
+    const next = document.querySelector('#next');
+    const previous = document.querySelector('#prev');
 
     let leftPosition = 0;
     let counter = 0;
     slider.style.width = totalWidth;
 
-    let timer = setInterval(slides, 2000);
-
-    function slides() {
+    next.addEventListener('click', function(event) {
         counter++;
-        if (counter == slideCount) {
+        if (counter === slideCount) {
             counter = 0;
+            leftPosition = 0;
             slider.style.left = leftPosition + 'px';
         }
         else {
-            leftPosition = `-${counter * slideWidth}px`;
-            slider.style.left = leftPosition;
+            leftPosition = `-${counter * slideWidth}`;
+            slider.style.left = leftPosition + 'px';
         }
-    }
-
-    slider.addEventListener('mouseover', function(){
-        clearInterval(timer)
     });
 
-    slider.addEventListener('mouseout', function(){
-        timer = setInterval(slides, 2000);
+    previous.addEventListener('click', function (event) {
+        event.preventDefault();
+        counter--;
+        if (counter < 0){
+            counter = slideCount - 1;
+            leftPosition = `-${counter * slideWidth}`;
+            slider.style.left = leftPosition + 'px';
+        }
+        else {
+            leftPosition = `-${counter * slideWidth}`;
+            slider.style.left = leftPosition + 'px';
+        }
     });
 });
